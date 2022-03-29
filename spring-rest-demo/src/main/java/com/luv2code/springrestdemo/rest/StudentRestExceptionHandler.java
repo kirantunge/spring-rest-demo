1c684f2caa0d01b929b547eb8c651e1793566f69
kiran.tunge <kiran.tunge@VSPL065.VATSA.LAN>
2022-03-29 19:11:38 +0530

    branch commit

--- spring-rest-demo/src/main/java/com/luv2code/springrestdemo/rest/StudentRestExceptionHandler.java
+++ spring-rest-demo/src/main/java/com/luv2code/springrestdemo/rest/StudentRestExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class StudentRestExceptionHandler {
 
+	//This is controller advice class
 	@ExceptionHandler
 	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception) {
 
